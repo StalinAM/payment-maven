4a7e5689df7e5e6be2b1be9232e64bb2bdd6321d
Stalin Acurio <sta_vla411@hotmail.com>
2024-12-01 17:49:20 -0500

    Add methods for updating, deleting and getting clients in ClientService

--- src/main/java/ec/edu/uce/payment/jpa/ClientService.java
+++ src/main/java/ec/edu/uce/payment/jpa/ClientService.java
@@ -2,9 +2,11 @@ package ec.edu.uce.payment.jpa;
 
 import jakarta.persistence.EntityManager;
 
+import java.util.ArrayList;
+
 public class ClientService {
 
-    private EntityManager em;
+    private final EntityManager em;
 
     public ClientService(EntityManager em) {
         this.em = em;
@@ -16,4 +18,23 @@ public class ClientService {
         em.getTransaction().commit();
     }
 
+    public void updateClient(Client client) {
+        em.getTransaction().begin();
+        em.merge(client);
+        em.getTransaction().commit();
+    }
+
+    public void deleteClient(Client client) {
+        em.getTransaction().begin();
+        em.remove(client);
+        em.getTransaction().commit();
+    }
+
+    public Client getClient(int id) {
+        return em.find(Client.class, id);
+    }
+
+    public void close() {
+        em.close();
+    }
 }
