3b830f61295d9ea94845ace37730096e2d89ec65
Stalin Acurio <sta_vla411@hotmail.com>
2024-12-01 04:10:18 -0500

    Add Client class with JPA mapping for client entity

---
+++ src/main/java/ec/edu/uce/payment/jpa/Client.java
@@ -0,0 +1,77 @@
+package ec.edu.uce.payment.jpa;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String cardNumber;
+    private String accountNumber;
+
+    public Client() {
+    }
+
+    public Client(int id, String name, String email, String password, String cardNumber, String accountNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.cardNumber = cardNumber;
+        this.accountNumber = accountNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+}
