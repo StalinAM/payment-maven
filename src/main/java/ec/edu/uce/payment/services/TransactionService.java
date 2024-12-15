32371f8fc6fe0d9bf0abbbb400dff364ea27b0da
Stalin Acurio <sta_vla411@hotmail.com>
2024-12-15 11:02:07 -0500

    Add methods to handle transactions by client and save transactions with multiple products

--- src/main/java/ec/edu/uce/payment/services/TransactionService.java
+++ src/main/java/ec/edu/uce/payment/services/TransactionService.java
@@ -1,5 +1,7 @@
 package ec.edu.uce.payment.services;
 
+import ec.edu.uce.payment.model.Client;
+import ec.edu.uce.payment.model.Product;
 import ec.edu.uce.payment.model.Transaction;
 import ec.edu.uce.payment.util.EntityManagerUtil;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -7,24 +9,22 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 
 import java.util.List;
+import java.util.Set;
 
 @ApplicationScoped
 public class TransactionService implements ICrud<Transaction> {
     private final EntityManager em= EntityManagerUtil.getEntityManager();
     EntityTransaction tx = em.getTransaction();
 
-    public TransactionService() {
-
-    }
-
     @Override
     public List<Transaction> list() {
         return em.createQuery("from Transaction", Transaction.class).getResultList();
     }
 
-    @Override
-    public Transaction findById(int id) {
-        return null;
+    public List<Transaction> getTransactionsByClient(Client client) {
+        return em.createQuery("SELECT t FROM Transaction t WHERE t.client = :client", Transaction.class)
+                .setParameter("client", client)
+                .getResultList();
     }
 
     @Override
@@ -33,14 +33,30 @@ public class TransactionService implements ICrud<Transaction> {
         em.persist(transaction);
         tx.commit();
     }
+    public void save(Transaction transaction, Set<Product> products) {
+        tx.begin();
+        transaction.setProducts(products);
+        em.persist(transaction);
+        tx.commit();
+    }
+
+    public Transaction createTransaction(Client client, Set<Product> products) {
+        Transaction transaction = new Transaction();
+        transaction.setClient(client);
+        transaction.setProducts(products);
+        transaction.setAmount(products.stream().mapToDouble(Product::getPrice).sum());
+        return transaction;
+    }
 
     @Override
     public void delete(int id) {
-
     }
 
     @Override
     public void update(Transaction transaction) {
-
+    }
+    @Override
+    public Transaction findById(int id) {
+        return null;
     }
 }
