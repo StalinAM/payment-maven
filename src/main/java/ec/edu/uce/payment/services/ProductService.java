94886336cf938b468fa5490ab28556ebd358ad60
Stalin Acurio <sta_vla411@hotmail.com>
2024-12-15 11:00:27 -0500

    Add method to find products by IDs in ProductService

--- src/main/java/ec/edu/uce/payment/services/ProductService.java
+++ src/main/java/ec/edu/uce/payment/services/ProductService.java
@@ -7,21 +7,21 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @ApplicationScoped
 public class ProductService implements ICrud<Product> {
     private final EntityManager em = EntityManagerUtil.getEntityManager();
     EntityTransaction tx = em.getTransaction();
-    public ProductService() {
 
+    public ProductService() {
     }
 
     @Override
     public List<Product> list() {
-
         return em.createQuery("from Product", Product.class).getResultList();
-
     }
 
     @Override
@@ -31,30 +31,39 @@ public class ProductService implements ICrud<Product> {
 
     @Override
     public void save(Product product) {
-
         tx.begin();
         em.persist(product);
         tx.commit();
-
     }
 
     @Override
     public void delete(int id) {
-
         Product product = em.find(Product.class, id);
         tx.begin();
         em.remove(product);
         tx.commit();
-
-
     }
 
     @Override
     public void update(Product product) {
-
         tx.begin();
         em.merge(product);
         tx.commit();
-
     }
+
+    public Set<Product> findProductsByIds(String productIds) {
+        Set<Product> products = new HashSet<>();
+        String[] productIdArray = productIds.split(",");
+
+        for (String id : productIdArray) {
+            int productId = Integer.parseInt(id);
+            Product product = findById(productId);
+            if (product != null) {
+                products.add(product);
+            }
+        }
+
+        return products;
+    }
+
 }
