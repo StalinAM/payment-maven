69bacdd6157453c1fce541ab5f883856b214e359
Stalin Acurio <sta_vla411@hotmail.com>
2024-12-15 11:02:00 -0500

    Update processTransaction method to accept multiple products and adjust transaction logic

--- src/main/java/ec/edu/uce/payment/services/PaymentService.java
+++ src/main/java/ec/edu/uce/payment/services/PaymentService.java
@@ -8,6 +8,8 @@ import ec.edu.uce.payment.model.Product;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+import java.util.Set;
+
 @ApplicationScoped
 public class PaymentService {
     @Inject
@@ -22,14 +24,14 @@ public class PaymentService {
     @QualifierPayment("Paypal")
     private IPayment paypalPayment;
 
-    public String processPayment(PaymentMethod paymentMethod, Client client, Product product) {
+    public String processPayment(PaymentMethod paymentMethod, Client client, Set<Product> products) {
         switch (paymentMethod.getName().toLowerCase()) {
             case "creditcard":
-                return creditCardPayment.pay(client, product);
+                return creditCardPayment.pay(client, products);
             case "banktransfer":
-                return bankTransferPayment.pay(client, product);
+                return bankTransferPayment.pay(client, products);
             case "paypal":
-                return paypalPayment.pay(client, product);
+                return paypalPayment.pay(client, products);
             default:
                 throw new IllegalArgumentException("Payment method not supported: " + paymentMethod.getName());
         }
