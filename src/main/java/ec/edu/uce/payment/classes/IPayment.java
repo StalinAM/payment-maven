ec9996da62ae3562be8d652648f4bde1ac948e18
Stalin Acurio <sta_vla411@hotmail.com>
2024-12-15 06:48:30 -0500

    Modify IPayment interface to accept customer and product parameters in payment method

--- src/main/java/ec/edu/uce/payment/classes/IPayment.java
+++ src/main/java/ec/edu/uce/payment/classes/IPayment.java
@@ -1,7 +1,10 @@
 package ec.edu.uce.payment.classes;
 
+import ec.edu.uce.payment.jpa.Client;
+import ec.edu.uce.payment.jpa.Product;
+
 public interface IPayment {
 
-    public void pay(double amount);
+    public String pay(Client client, Product product);
 
 }
