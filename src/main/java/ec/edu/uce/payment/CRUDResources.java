a23b93fab4f961f4f76f84f5a5d98eef90ea9b6c
Stalin Acurio <sta_vla411@hotmail.com>
2024-12-15 12:35:39 -0500

    refactor code

--- src/main/java/ec/edu/uce/payment/CRUDResources.java
+++ src/main/java/ec/edu/uce/payment/CRUDResources.java
@@ -35,103 +35,13 @@ public class CRUDResources {
     @GET
     @Produces("text/plain")
     @Path("/init-data/create")
-    public String createPaymentMethod() {
+    public String initData() {
 
-        if (paymentMethodService.list().isEmpty() && clientService.list().isEmpty() && productService.list().isEmpty()) {
-            PaymentMethod paymentMethod1 = new PaymentMethod();
-            paymentMethod1.setName("creditcard");
-
-            PaymentMethod paymentMethod2 = new PaymentMethod();
-            paymentMethod2.setName("paypal");
-
-            PaymentMethod paymentMethod3 = new PaymentMethod();
-            paymentMethod3.setName("banktransfer");
-
-            // Guardar los tres métodos de pago
-            paymentMethodService.save(paymentMethod1);
-            paymentMethodService.save(paymentMethod2);
-            paymentMethodService.save(paymentMethod3);
-
-            Client client1 = new Client();
-            client1.setName("John Doe");
-            client1.setEmail("john.doe@email.com");
-            clientService.save(client1);
-
-            Client client2 = new Client();
-            client2.setName("Jane Smith");
-            client2.setEmail("jane.smith@email.com");
-            clientService.save(client2);
-
-            Client client3 = new Client();
-            client3.setName("Carlos Rivera");
-            client3.setEmail("carlos.rivera@email.com");
-            clientService.save(client3);
-
-            Client client4 = new Client();
-            client4.setName("Maria Gómez");
-            client4.setEmail("maria.gomez@email.com");
-            clientService.save(client4);
-
-            Client client5 = new Client();
-            client5.setName("Luis Martínez");
-            client5.setEmail("luis.martinez@email.com");
-            clientService.save(client5);
-
-            Product product1 = new Product();
-            product1.setName("Café Espresso");
-            product1.setPrice(10.5);
-            productService.save(product1);
-
-            Product product2 = new Product();
-            product2.setName("Café Latte");
-            product2.setPrice(12.0);
-            productService.save(product2);
-
-            Product product3 = new Product();
-            product3.setName("Café Americano");
-            product3.setPrice(8.5);
-            productService.save(product3);
+        if (isDatabaseEmpty()) {
+            createInitialData();
             return "Datos iniciales subidos!";
         } else {
-
-            List<Client> allClients = clientService.list();
-            List<Product> allProducts = productService.list();
-            List<PaymentMethod> listPaymentMethods = paymentMethodService.list();
-
-            // Generamos la salida para cada lista
-            StringBuilder result = new StringBuilder();
-
-            // Mostrar los clientes
-            result.append("Clientes: ").append(allClients.size()).append("\n")
-                    .append("-------------------------------\n");
-            for (Client client : allClients) {
-                result.append("ID: ").append(client.getId())
-                        .append(", Nombre: ").append(client.getName())
-                        .append(", Correo: ").append(client.getEmail())
-                        .append("\n");
-            }
-
-            // Mostrar los productos
-            result.append("\nProductos: ").append(allProducts.size()).append("\n")
-                    .append("-------------------------------\n");
-            for (Product product : allProducts) {
-                result.append("ID: ").append(product.getId())
-                        .append(", Nombre: ").append(product.getName())
-                        .append(", Precio: ").append(product.getPrice())
-                        .append("\n");
-            }
-
-            // Mostrar los métodos de pago
-            result.append("\nMétodos de pago: ").append(listPaymentMethods.size()).append("\n")
-                    .append("-------------------------------\n");
-            for (PaymentMethod paymentMethod : listPaymentMethods) {
-                result.append("ID: ").append(paymentMethod.getId())
-                        .append(", Nombre: ").append(paymentMethod.getName())
-                        .append("\n");
-            }
-
-            return result.toString();
-
+            return getDatabaseInfo();
         }
 
     }
@@ -139,26 +49,33 @@ public class CRUDResources {
     @GET
     @Produces("text/plain")
     @Path("/client/create")
-    public String createClient() {
+    public String createClient(@QueryParam("name") String name, @QueryParam("email") String email) {
+        if (name == null || email == null) {
+            return "Debe proporcionar nombre y correo electrónico.";
+        }
         Client client = new Client();
-        client.setName("John Doe");
-        client.setEmail("example@email.com");
+        client.setName(name);
+        client.setEmail(email);
         clientService.save(client);
-
         return "Cliente " + client.getName() + " creado!";
     }
 
     @GET
     @Produces("text/plain")
     @Path("/client/update")
-    public String updateClient() {
-        Client client = new Client();
-        client.setId(1);
-        client.setName("John Does");
-        client.setEmail("example@email.com");
-        clientService.update(client);
-
-        return "Cliente " + client.getName() + " actualizado!";
+    public String updateClient(@QueryParam("id") int id, @QueryParam("name") String name, @QueryParam("email") String email) {
+        if (id == 0 || name == null || email == null) {
+            return "Debe proporcionar ID, nombre y correo electrónico.";
+        }
+        Client client = clientService.findById(id);
+        if (client != null) {
+            client.setName(name);
+            client.setEmail(email);
+            clientService.update(client);
+            return "Cliente " + client.getName() + " actualizado!";
+        } else {
+            return "Cliente no encontrado!";
+        }
     }
 
     @GET
@@ -183,13 +100,15 @@ public class CRUDResources {
     @GET
     @Produces("text/plain")
     @Path("/product/create")
-    public String createProduct() {
+    public String createProduct(@QueryParam("name") String name, @QueryParam("price") double price) {
+        if (name == null || price == 0) {
+            return "Debe proporcionar nombre y precio del producto.";
+        }
         Product product = new Product();
-        product.setName("Cafe");
-        product.setPrice(20.5);
+        product.setName(name);
+        product.setPrice(price);
         productService.save(product);
-
-        return "Product " + product.getName() + " creado!";
+        return "Producto " + product.getName() + " creado!";
     }
 
     @GET
@@ -201,7 +120,7 @@ public class CRUDResources {
         if (product == null) {
             return "Producto no encontrado!";
         }
-        return product.getName();
+        return "Producto " + product.getName() + " encontrado!";
     }
 
     @GET
@@ -215,24 +134,77 @@ public class CRUDResources {
     @GET
     @Produces("text/plain")
     @Path("/product/update")
-    public String updateProduct() {
-        Product product = new Product();
-        product.setId(1);
-        product.setName("Cafes");
-        product.setPrice(10.5);
-        productService.update(product);
-        return "Product " + product.getName() + " actualizado!";
-    }
-
-    @GET
-    @Produces("text/plain")
-    @Path("/product/delete")
-    public String deleteProduct() {
-        Product product = productService.findById(1);
-        if (product == null) {
+    public String updateProduct(@QueryParam("id") int id, @QueryParam("name") String name, @QueryParam("price") double price) {
+        if (id == 0 || name == null || price == 0) {
+            return "Debe proporcionar ID, nombre y precio del producto.";
+        }
+        Product product = productService.findById(id);
+        if (product != null) {
+            product.setName(name);
+            product.setPrice(price);
+            productService.update(product);
+            return "Producto " + product.getName() + " actualizado!";
+        } else {
             return "Producto no encontrado!";
         }
-        return product.getName() + " eliminado!";
+    }
+
+    private boolean isDatabaseEmpty() {
+        return paymentMethodService.list().isEmpty() && clientService.list().isEmpty() && productService.list().isEmpty();
+    }
+
+    private void createInitialData() {
+        paymentMethodService.save(new PaymentMethod("creditcard"));
+        paymentMethodService.save(new PaymentMethod("paypal"));
+        paymentMethodService.save(new PaymentMethod("banktransfer"));
+
+        clientService.save(new Client("John Doe", "john.doe@email.com"));
+        clientService.save(new Client("Jane Smith", "jane.smith@email.com"));
+        clientService.save(new Client("Carlos Rivera", "carlos.rivera@email.com"));
+        clientService.save(new Client("Maria Gómez", "maria.gomez@email.com"));
+        clientService.save(new Client("Luis Martínez", "luis.martinez@email.com"));
+
+        productService.save(new Product("Café Espresso", 10.5));
+        productService.save(new Product("Café Latte", 12.0));
+        productService.save(new Product("Café Americano", 8.5));
+    }
+
+    private String getDatabaseInfo() {
+        List<Client> allClients = clientService.list();
+        List<Product> allProducts = productService.list();
+        List<PaymentMethod> listPaymentMethods = paymentMethodService.list();
+        StringBuilder result = new StringBuilder();
+
+        // Mostrar los clientes
+        result.append("Clientes: ").append(allClients.size()).append("\n")
+                .append("-------------------------------\n");
+        for (Client client : allClients) {
+            result.append("ID: ").append(client.getId())
+                    .append(", Nombre: ").append(client.getName())
+                    .append(", Correo: ").append(client.getEmail())
+                    .append("\n");
+        }
+
+        // Mostrar los productos
+        result.append("\nProductos: ").append(allProducts.size()).append("\n")
+                .append("-------------------------------\n");
+        for (Product product : allProducts) {
+            result.append("ID: ").append(product.getId())
+                    .append(", Nombre: ").append(product.getName())
+                    .append(", Precio: ").append(product.getPrice())
+                    .append("\n");
+        }
+
+        // Mostrar los métodos de pago
+        result.append("\nMétodos de pago: ").append(listPaymentMethods.size()).append("\n")
+                .append("-------------------------------\n");
+        for (PaymentMethod paymentMethod : listPaymentMethods) {
+            result.append("ID: ").append(paymentMethod.getId())
+                    .append(", Nombre: ").append(paymentMethod.getName())
+                    .append("\n");
+        }
+
+        return result.toString();
     }
 
 }
